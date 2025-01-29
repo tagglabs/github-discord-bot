db75a56a7d8badedc002000998250cfd5930826f
qbikle <mvg2162@gmail.com>
2025-01-29 18:46:57 +0530

    refactor: convert github_api_request to async and update related commands

--- bot.py
+++ bot.py
@@ -14,34 +14,42 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 github_org = 'tagglabs'
 import requests
 
-def github_api_request(endpoint, method='GET', data=None):
+async def github_api_request(endpoint, method='GET', data=None):
     url = f'https://api.github.com{endpoint}'
     headers = {'Authorization': f'token {GITHUB_TOKEN}'}
     response = requests.request(method, url, headers=headers, json=data)
-    return response.json()
+    response.raise_for_status()
+    if response.headers.get('content-type') == 'application/json; charset=utf-8':
+        return response.json()
+
 
 @bot.command()
 async def list_repos(ctx):
 #   List all repos in the github org along with their urls
-    repos = github_api_request(f'/orgs/{github_org}/repos')
+    repos = await github_api_request(f'/orgs/{github_org}/repos')
     for repo in repos:
         await ctx.send(f'{repo["name"]}: {repo["html_url"]}')
 
 @bot.command()
 async def get_repo(ctx, repo_name):
-    repo = github_api_request(f'/repos/{github_org}/{repo_name}')
+    repo = await github_api_request(f'/repos/{github_org}/{repo_name}')
     await ctx.send(repo['html_url'])
 
 @bot.command()
 async def search_repos(ctx, search_term):
-    repos = github_api_request(f'/search/repositories?q={search_term}+org:{github_org}')
+    repos = await github_api_request(f'/search/repositories?q={search_term}+org:{github_org}')
     for repo in repos['items']:
         await ctx.send(f'{repo["name"]}: {repo["html_url"]}')
         
 @bot.command()
 async def create_repo(ctx, repo_name):
-    data = {'name': repo_name}
-    repo = github_api_request(f'/orgs/{github_org}/repos', method='POST', data=data)
-    await ctx.send(repo['html_url'])
-
+    data = {'name': repo_name, 'has_projects': True, 'team_id': 9478163}
+    repo = await github_api_request(f'/orgs/{github_org}/repos', method='POST', data=data)
+    team_repo_data = {
+        'permission': 'maintain'
+    }
+    #/orgs/{org}/teams/{team_slug}/repos/{owner}/{repo}
+    await github_api_request(f'/orgs/{github_org}/teams/campaigns/repos/{github_org}/{repo_name}', method='PUT', data=team_repo_data)
+    await ctx.send(f'Repo created: {repo["html_url"]} with write access for the team.')
+    
 bot.run(DISCORD_TOKEN)
